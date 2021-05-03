a4bbd291ba9c517d31007cbc036fdeb0250e4b52
David L. Whitehurst <dwhitehurst@ms3-inc.com>
2021-05-03 08:56:50 -0400

    progressing application by designing API in swagger

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // swagger api documentation
-//const swaggerUi = require("swagger-ui-express"),
-//    swaggerDocument = require("./swagger.json");
+const swaggerUi = require("swagger-ui-express"),
+    swaggerDocument = require("./swagger.json");
 
 // simple route
 app.get("/", (req, res) => {
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 //require("./app/routes/recipe.routes.js")(app);
 
 // swagger path to api documentation
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
